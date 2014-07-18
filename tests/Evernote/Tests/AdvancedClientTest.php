13db0697406c1c2d80149790ac45da2fc825fde9
Laurent Sarrazin <laurent@sarrazin.io>
2014-07-18 16:58:55 +0200

    Setup tests

---
+++ tests/Evernote/Tests/AdvancedClientTest.php
@@ -0,0 +1,8 @@
+<?php
+namespace Evernote\Tests;
+
+use Evernote\AdvancedClient;
+
+class AdvancedClientTest extends \PHPUnit_Framework_TestCase
+{
+}
---
+++ tests/bootstrap.php
@@ -0,0 +1,2 @@
+<?php
+include_once __DIR__ . '/../src/autoload.php';
