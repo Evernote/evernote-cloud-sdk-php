2af87775dc48cf11e2153d62c696d6d9420d2c4a
Laurent Sarrazin <laurent@sarrazin.io>
2014-07-19 22:54:18 +0200

    Simplify the Advanced client

--- src/Evernote/AdvancedClient.php
+++ src/Evernote/AdvancedClient.php
@@ -2,108 +2,54 @@
 
 namespace Evernote;
 
+use Evernote\Factory\ThriftClientFactory;
 use Evernote\Store;
 
 class AdvancedClient
 {
-    private $consumerKey;
-    private $consumerSecret;
-    private $sandbox;
-    private $serviceHost;
-    private $additionalHeaders;
-    private $token;
-    private $secret;
+    protected $sandbox;
 
-    public function __construct($options)
+    protected $userStore;
+
+    const SANDBOX_BASE_URL = 'https://sandbox.evernote.com';
+    const PROD_BASE_URL    = 'https://www.evernote.com';
+
+    public function __construct($sandbox = true)
     {
-        $this->consumerKey = isset($options['consumerKey']) ? $options['consumerKey'] : null;
-        $this->consumerSecret = isset($options['consumerSecret']) ? $options['consumerSecret'] : null;
-
-        $options += array('sandbox' => true);
-        $this->sandbox = $options['sandbox'];
-
-        $defaultServiceHost = $this->sandbox ? 'sandbox.evernote.com' : 'www.evernote.com';
-
-        $options += array('serviceHost' => $defaultServiceHost);
-        $this->serviceHost = $options['serviceHost'];
-
-        $options += array('additionalHeaders' => array());
-        $this->additionalHeaders = $options['additionalHeaders'];
-
-        $this->token = isset($options['token']) ? $options['token'] : null;
-        $this->secret = isset($options['secret']) ? $options['secret'] : null;
-    }
-
-    public function getRequestToken($callbackUrl)
-    {
-        $oauth = new \OAuth($this->consumerKey, $this->consumerSecret);
-
-        return $oauth->getRequestToken($this->getEndpoint('oauth'), $callbackUrl);
-    }
-
-    public function getAccessToken($oauthToken, $oauthTokenSecret, $oauthVerifier)
-    {
-        $oauth = new \OAuth($this->consumerKey, $this->consumerSecret);
-        $oauth->setToken($oauthToken, $oauthTokenSecret);
-        $accessToken= $oauth->getAccessToken($this->getEndpoint('oauth'), null, $oauthVerifier);
-
-        $this->token = $accessToken['oauth_token'];
-
-        return $accessToken;
-    }
-
-    public function getAuthorizeUrl($requestToken)
-    {
-        $url = $this->getEndpoint('OAuth.action');
-        $url .= '?oauth_token=';
-        $url .= urlencode($requestToken);
-
-        return $url;
+        $this->sandbox = $sandbox;
     }
 
     public function getUserStore()
     {
-        $userStoreUrl = $this->getEndpoint('/edam/user');
+        if (null === $this->userStore) {
+            $this->userStore =
+                $this->getThriftClient('user', $this->getEndpoint('/edam/user'));
+        }
 
-        return new Store($this->token, '\EDAM\UserStore\UserStoreClient', $userStoreUrl);
+        return $this->userStore;
     }
 
-    public function getNoteStore()
+    public function getNoteStore($noteStoreUrl)
     {
-        $userStore = $this->getUserStore();
-        $noteStoreUrl = $userStore->getNoteStoreUrl();
-
-        return new Store($this->token, '\EDAM\NoteStore\NoteStoreClient', $noteStoreUrl);
-    }
-
-    public function getSharedNoteStore($linkedNotebook)
-    {
-        $noteStoreUrl = $linkedNotebook->noteStoreUrl;
-        $noteStore = new Store($this->token, '\EDAM\NoteStore\NoteStoreClient', $noteStoreUrl);
-        $sharedAuth = $noteStore->authenticateToSharedNotebook($linkedNotebook->shareKey);
-        $sharedToken = $sharedAuth->authenticationToken;
-
-        return new Store($sharedToken, '\EDAM\NoteStore\NoteStoreClient', $noteStoreUrl);
-    }
-
-    public function getBusinessNoteStore()
-    {
-        $userStore = $this->getUserStore();
-        $bizAuth = $userStore->authenticateToBusiness();
-        $bizToken = $bizAuth->authenticationToken;
-        $noteStoreUrl = $bizAuth->noteStoreUrl;
-
-        return new Store($bizToken, '\EDAM\NoteStore\NoteStoreClient', $noteStoreUrl);
+        return $this->getThriftClient('note', $noteStoreUrl);
     }
 
     protected function getEndpoint($path = null)
     {
-        $url = "https://".$this->serviceHost;
-        if ($path != null) {
-            $url .= "/".$path;
+        $url = $this->sandbox ? self::SANDBOX_BASE_URL : self::PROD_BASE_URL;
+
+        if (null != $path) {
+            $url .= '/' . $path;
         }
 
         return $url;
+    }
+
+    protected function getThriftClient($type, $url)
+    {
+        $factory = new ThriftClientFactory();
+
+        return $factory->createThriftClient($type, $url);
     }
 
 }
