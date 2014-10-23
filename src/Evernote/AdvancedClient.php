d1759603dc17d191f907459bd39b363efc71dd3e
Laurent Sarrazin <laurent@sarrazin.io>
2014-10-23 22:27:12 +0200

    Add inline doc

--- src/Evernote/AdvancedClient.php
+++ src/Evernote/AdvancedClient.php
@@ -7,8 +7,10 @@ use Evernote\Store;
 
 class AdvancedClient
 {
+    /** @var bool  */
     protected $sandbox;
 
+    /** @var \Evernote\Factory\ThriftClientFactory  */
     protected $thriftClientFactory;
 
     /** @var  \EDAM\UserStore\UserStoreClient */
@@ -17,6 +19,10 @@ class AdvancedClient
     const SANDBOX_BASE_URL = 'https://sandbox.evernote.com';
     const PROD_BASE_URL    = 'https://www.evernote.com';
 
+    /**
+     * @param bool $sandbox
+     * @param null $thriftClientFactory
+     */
     public function __construct($sandbox = true, $thriftClientFactory = null)
     {
         $this->sandbox             = $sandbox;
@@ -24,6 +30,9 @@ class AdvancedClient
         $this->thriftClientFactory = $thriftClientFactory;
     }
 
+    /**
+     * @return \EDAM\UserStore\UserStoreClient
+     */
     public function getUserStore()
     {
         if (null === $this->userStore) {
@@ -34,11 +43,19 @@ class AdvancedClient
         return $this->userStore;
     }
 
+    /**
+     * @param $noteStoreUrl
+     * @return mixed
+     */
     public function getNoteStore($noteStoreUrl)
     {
         return $this->getThriftClient('note', $noteStoreUrl);
     }
 
+    /**
+     * @param null $path
+     * @return string
+     */
     public function getEndpoint($path = null)
     {
         $url = $this->sandbox ? self::SANDBOX_BASE_URL : self::PROD_BASE_URL;
@@ -50,6 +67,9 @@ class AdvancedClient
         return $url;
     }
 
+    /**
+     * @return ThriftClientFactory
+     */
     public function getThriftClientFactory()
     {
         if (null === $this->thriftClientFactory) {
@@ -58,7 +78,12 @@ class AdvancedClient
 
         return $this->thriftClientFactory;
     }
-    
+
+    /**
+     * @param $type
+     * @param $url
+     * @return mixed
+     */
     protected function getThriftClient($type, $url)
     {
         return $this->getThriftClientFactory()->createThriftClient($type, $url);
