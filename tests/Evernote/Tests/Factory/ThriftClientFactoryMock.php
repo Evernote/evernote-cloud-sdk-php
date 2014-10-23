64e96e709d67d27b34517a5bb1fedef565ce7876
Laurent Sarrazin <laurent@sarrazin.io>
2014-10-23 16:28:46 +0200

    Clean the tests

--- tests/Evernote/Tests/Factory/ThriftClientFactoryMock.php
+++ tests/Evernote/Tests/Factory/ThriftClientFactoryMock.php
@@ -4,3 +4,58 @@ namespace Evernote\Tests\Factory;
 
 use Evernote\Factory\ThriftClientFactory;
 
+class ThriftClientFactoryMock
+{
+    protected $token;
+    protected $useFixtures;
+
+    public function __construct($token, $useFixtures = true)
+    {
+        $this->token       = $token;
+        $this->useFixtures = $useFixtures;
+    }
+
+    public function createThriftClient($type, $url)
+    {
+        return new ThriftClient($type, $url, $this->token, $this->useFixtures);
+    }
+}
+
+class ThriftClient
+{
+    protected $type;
+    protected $url;
+    protected $token;
+
+    public function __construct($type, $url, $token, $useFixtures)
+    {
+        $this->type        = $type;
+        $this->url         = $url;
+        $this->token       = $token;
+        $this->useFixtures = $useFixtures;
+    }
+
+    public function __call($name, $args)
+    {
+        $file = __DIR__ . '/../../../fixtures/' . $this->type . sha1($this->token . $this->url . $name . implode('', $args));
+
+        echo $file;
+
+        if ($this->useFixtures && is_file($file) && is_readable($file)) {
+            $response = unserialize(file_get_contents($file));
+
+            if (!empty($response)) {
+                return $response;
+            }
+        }
+
+        $realThriftClientFactory = new ThriftClientFactory();
+        $client = $realThriftClientFactory->createThriftClient($this->type, $this->url);
+
+        $response = call_user_func_array(array($client, $name), $args);
+
+        file_put_contents($file, serialize($response));
+
+        return $response;
+    }
+}
