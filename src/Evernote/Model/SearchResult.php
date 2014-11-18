b0d688f45c55f32b62140e21abd94cd6ed95be75
Laurent Sarrazin <laurent@sarrazin.io>
2014-11-18 13:17:48 +0100

    Add SearchResult

---
+++ src/Evernote/Model/SearchResult.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Evernote\Model;
+
+class SearchResult
+{
+    public $guid;
+
+    public $type;
+
+    public $notebook;
+
+    public $title;
+
+    public $created;
+
+    public $updated;
+} 
