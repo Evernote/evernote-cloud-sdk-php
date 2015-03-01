603e97839c2741de4eea74a39f99586032ab1660
Laurent Sarrazin <laurent@sarrazin.io>
2015-03-01 15:01:46 +0100

    Add xml declaration if not given

--- src/Evernote/Model/EnmlNoteContent.php
+++ src/Evernote/Model/EnmlNoteContent.php
@@ -3,10 +3,21 @@
 namespace Evernote\Model;
 
 use Evernote\Enml\Converter\EnmlToHtmlConverter;
+use Evernote\Enml\Converter\HtmlConverterInterface;
 
 class EnmlNoteContent extends NoteContent implements NoteContentInterface
 {
     protected $htmlConverter;
+
+    public function __construct($content, HtmlConverterInterface $htmlConverter = null)
+    {
+        if (!$this->hasXmlDeclaration($content)) {
+            $content = $this->decorate($content);
+        }
+
+        $this->content   = $content;
+        $this->htmlConverter = $htmlConverter;
+    }
 
     public function getEnmlConverter()
     {
@@ -39,6 +50,21 @@ class EnmlNoteContent extends NoteContent implements NoteContentInterface
         return <<<HTLM
 <html><body>$html</body></html>
 HTLM;
+
+    }
+
+    protected function hasXmlDeclaration($content)
+    {
+        return substr(trim($content), 0, 5) == '<?xml';
+    }
+
+    protected function decorate($content)
+    {
+        return <<<ENML
+<?xml version='1.0' encoding='utf-8'?>
+<!DOCTYPE en-note SYSTEM "http://xml.evernote.com/pub/enml2.dtd">
+<en-note>$content</en-note>
+ENML;
 
     }
 }
